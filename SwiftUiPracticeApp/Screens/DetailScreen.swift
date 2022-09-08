d193aed0a7d66cc5095dd8313fd7c3defd2350d5
waqas.khadim <waqas.khadim@invozone.com>
2022-09-08 12:21:06 +0500

    initial commit

---
+++ SwiftUiPracticeApp/Screens/DetailScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DetailScreen.swift
+//  SwiftUiPracticeApp
+//
+//  Created by Waqas Khadim on 08/09/2022.
+//
+
+import SwiftUI
+
+struct DetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailScreen()
+    }
+}
